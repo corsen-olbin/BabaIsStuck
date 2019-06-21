278bc472fe796be3b92c270d4ea9033dc5b69443
Corbin Olsen <corbinolsen97@gmail.com>
2019-06-20 22:46:51 -0500

    Initial commit, added few words, can make most of the combinations with those words. Still will not do combos in form NOUN IS NOUN

---
+++ BabaIsStuck/BabaIsStuck/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BabaIsStuck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, RoutedEventArgs e)
+        {
+            var nouns = stkNouns.Children.OfType<CheckBox>().Where(x => x.IsChecked.Value).Select(x => x.Content.ToString());
+            var prepostions = stkPrepositions.Children.OfType<CheckBox>().Where(x => x.IsChecked.Value).Select(x => x.Content.ToString());
+            var verbs = stkVerbs.Children.OfType<CheckBox>().Where(x => x.IsChecked.Value).Select(x => x.Content.ToString());
+
+            var combos = from n in nouns
+                         from p in prepostions
+                         from v in verbs
+                         select $"{n} {p} {v}";
+
+            StringBuilder sb = new StringBuilder();
+            foreach(string combo in combos)
+            {
+                sb.AppendLine(combo);
+            }
+            tbxSolutions.Text = sb.ToString();
+        }
+    }
+}
